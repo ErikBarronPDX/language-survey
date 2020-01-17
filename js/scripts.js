3472195831fa6a00fdf95ab3f09f231fef5583cf
Erik Barron <todayisagrnday21@gmail.com>
2020-01-17 10:28:07 -0800

    create a separate branch to come back to for dynamic question text

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,42 @@
-var questions = [1, 2, 3, 4, 5];
+var questions = [1, 2, 3];
+var language1Counter = 1;
+var language2Counter = 1;
+var language3Counter = 1;
+var language4Counter = 1;
 
 $(document).ready(function() {
   $("#submit").click(function() {
 
     
     
-    questions.forEach(function(repeatTimes) {
-
-      alert(repeatTimes);
-      
-
-    })
+    questions.forEach(function(repeatTimes, event) {
 
     var surveyResult = $("input:radio[name=surveyQuestion]:checked").val();
 
+    if (surveyResult === 1){
+      language1Counter++;
+    } else if (surveyResult === 2){
+      language2Counter++;
+    } else if (surveyResult === 3) {
+      language3Counter++;
+    } else {
+      language4Counter++;
+    }
+  
     alert(surveyResult);
+
+    checkResult(surveyResult);
+
+
+    })
+
+
+    
 
   })
 })
+
+function checkResult() {
+  
+
+}
