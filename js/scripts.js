e7d9c763d5ee2fdb96d24aec227119977a5244c3
Erik Barron <todayisagrnday21@gmail.com>
2020-01-17 11:40:41 -0800

    create a save point before attempting to refactor code using for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
 var questions = [1, 2, 3];
-var language1Counter = 1;
-var language2Counter = 1;
-var language3Counter = 1;
-var language4Counter = 1;
+var language1Counter = 0;
+var language2Counter = 0;
+var language3Counter = 0;
+var language4Counter = 0;
 
 $(document).ready(function() {
-  $("#submit").click(function() {
 
-    
-    
-    questions.forEach(function(repeatTimes, event) {
+  
 
-    var surveyResult = $("input:radio[name=surveyQuestion]:checked").val();
+  $("#submitOne").click(function() {
+    var surveyResult = $("input:radio[name=surveyQuestionOne]:checked").val();
+    if (surveyResult === "1"){
+      language1Counter++;
+    } else if (surveyResult === 2){
+      language2Counter++;
+    } else if (surveyResult === 3) {
+      language3Counter++;
+    } else {
+      language4Counter++;
+    }
+    $("#questionOne").hide();
+    $("#questionTwo").show();
+    alert(surveyResult);
+    $("#spanOne").text(language1Counter);
+    $("#spanTwo").text(language2Counter);
+    $("#spanThree").text(language3Counter);
+    $("#spanFour").text(language4Counter);
+  })
 
+  $("#submitTwo").click(function() {
+    var surveyResult = $("input:radio[name=surveyQuestionTwo]:checked").val();
     if (surveyResult === 1){
       language1Counter++;
     } else if (surveyResult === 2){
@@ -22,18 +39,68 @@ $(document).ready(function() {
     } else {
       language4Counter++;
     }
-  
+    $("#questionTwo").hide();
+    $("#questionThree").show();
     alert(surveyResult);
-
-    checkResult(surveyResult);
-
-
-    })
-
-
-    
-
+    $("#spanOne").text(language1Counter);
+    $("#spanTwo").text(language2Counter);
+    $("#spanThree").text(language3Counter);
+    $("#spanFour").text(language4Counter);
   })
+
+  $("#submitThree").click(function() {
+    var surveyResult = $("input:radio[name=surveyQuestionThree]:checked").val();
+    if (surveyResult === 1){
+      language1Counter++;
+    } else if (surveyResult === 2){
+      language2Counter++;
+    } else if (surveyResult === 3) {
+      language3Counter++;
+    } else {
+      language4Counter++;
+    }
+    $("#questionThree").hide();
+    $("#questionFour").show();
+    alert(surveyResult);
+  })
+
+  $("#submitFour").click(function() {
+    var surveyResult = $("input:radio[name=surveyQuestionFour]:checked").val();
+    if (surveyResult === 1){
+      language1Counter++;
+    } else if (surveyResult === 2){
+      language2Counter++;
+    } else if (surveyResult === 3) {
+      language3Counter++;
+    } else {
+      language4Counter++;
+    }
+    $("#questionFour").hide();
+    $("#questionFive").show();
+    alert(surveyResult);
+  })
+
+
+  $("#submitFive").click(function() {
+    var surveyResult = $("input:radio[name=surveyQuestionFive]:checked").val();
+    if (surveyResult === 1){
+      language1Counter++;
+    } else if (surveyResult === 2){
+      language2Counter++;
+    } else if (surveyResult === 3) {
+      language3Counter++;
+    } else {
+      language4Counter++;
+    }
+    $("#questionFive").hide();
+    alert(surveyResult);
+  })
+
+  $("#spanOne").text(language1Counter);
+  $("#spanTwo").text(language2Counter);
+  $("#spanThree").text(language3Counter);
+  $("#spanFour").text(language4Counter);
+
 })
 
 function checkResult() {
