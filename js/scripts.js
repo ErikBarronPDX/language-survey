cb6578a375d76ae8fca9a51fe940b2ce1df571f4
Erik Barron <todayisagrnday21@gmail.com>
2020-01-17 16:39:54 -0800

    include a form for a name input, results contain user name

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,47 @@ var language1Counter = 0;
 var language2Counter = 0;
 var language3Counter = 0;
 var language4Counter = 0;
+
 $(document).ready(function() {
-  questions.forEach(function(questions) {
-    $("#submit" + questions).click(function() {
-      var surveyResult = $("input:radio[name=surveyQuestion" + questions + "]:checked").val();
-      if (!surveyResult) {
-        alert("please select an answer")
-      } else {
-      if (surveyResult === "1")
-        language1Counter++;
-        else if (surveyResult === "2")
-        language2Counter++;
-        else if (surveyResult === "3")
-        language3Counter++;
-        else
-        language4Counter++;
-      $("#question" + questions).hide();
-      $("#question" + (questions + 1)).fadeIn();
-      if (questions === 5) {
-        $("#displayResults").show();
-        if (language1Counter >= language2Counter && language1Counter >= language3Counter && language1Counter >= language4Counter)
-          $("#htmlResult").show();
-        else if (language2Counter >= language1Counter && language2Counter >= language3Counter && language2Counter >= language4Counter)
-          $("#javaScriptResult").show();
-        else if (language3Counter >= language1Counter && language3Counter >= language2Counter && language3Counter >= language4Counter)
-          $("#pythonResult").show();
-        else 
-          $("#reactResult").show();
-      }
+  $("#questionName").submit(function(event) {
+    var userName = $("#userName").val();
+    if (!userName) {
+      alert("Please enter your name");
+    } else {
+      $(".userName").text(userName);
+      $("#questionName").hide();
+      $("#question1").fadeIn();
+      event.preventDefault();
+      questions.forEach(function(questions) {
+        $("#submit" + questions).click(function() {
+          var surveyResult = $("input:radio[name=surveyQuestion" + questions + "]:checked").val();
+          if (!surveyResult) {
+            alert("please select an answer")
+          } else {
+          if (surveyResult === "1")
+            language1Counter++;
+            else if (surveyResult === "2")
+            language2Counter++;
+            else if (surveyResult === "3")
+            language3Counter++;
+            else
+            language4Counter++;
+          $("#question" + questions).hide();
+          $("#question" + (questions + 1)).fadeIn();
+          if (questions === 5) {
+            $("#displayResults").show();
+            if (language1Counter >= language2Counter && language1Counter >= language3Counter && language1Counter >= language4Counter)
+              $("#htmlResult").show();
+            else if (language2Counter >= language1Counter && language2Counter >= language3Counter && language2Counter >= language4Counter)
+              $("#javaScriptResult").show();
+            else if (language3Counter >= language1Counter && language3Counter >= language2Counter && language3Counter >= language4Counter)
+              $("#pythonResult").show();
+            else 
+              $("#reactResult").show();
+          }
+        }
+        })
+      });
     }
-    })
-  });
+  })
 });
